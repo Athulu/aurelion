43b14d66661032ff796262c3a2bd4971469f3960
Athulu <nikodem1703@gmail.com>
2025-07-12 19:21:42 +0200

    initialize project

---
+++ src/main/java/aurelion/AurelionApplication.java
@@ -0,0 +1,13 @@
+package aurelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurelionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AurelionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aurelion/AurelionApplicationTests.java
@@ -0,0 +1,13 @@
+package aurelion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurelionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
